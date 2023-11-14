a751598e44d6b5519e013c47857180d96e4ec1f0
ISM6225 <ISM6225Class@gmail.com>
2023-11-13 21:22:00 -0500

    Add project files.

---
+++ Linq_Practice_student_7/DataAccess/SchoolDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Linq_Practice_student_7.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Linq_Practice_student_7.DataAccess
+{
+    public class SchoolDbContext : DbContext
+    {
+        public SchoolDbContext(DbContextOptions<SchoolDbContext> options) : base(options) { }
+
+        public DbSet<College> Colleges { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
+
